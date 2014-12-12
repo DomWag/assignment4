ef5147a2ca1fe2b2c55c11d9721400ff0a939d8c
Dominik Wagner <wagnerdo@fim.uni-passau.de>
2014-12-12 22:29:37 +0100

    first build of certainbookstorereplicator according to http://kamleshkumar.com/2009/10/02/java-threads-callable-and-future/

--- acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
+++ acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
@@ -3,7 +3,9 @@ package com.acertainbookstore.business;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -23,11 +25,20 @@ public class CertainBookStoreReplicator implements Replicator {
 			ReplicationRequest request) {
 		List<Future<ReplicationResult>> replication = new ArrayList<Future<ReplicationResult>>();
 		for(String s: slaveServers){
+			//Create the instance of the Callable task
+			Callable<ReplicationResult> rs = new CertainBookStoreReplicationTask();
+			//create the object of FutureTask
+			FutureTask<ReplicationResult> task = new FutureTask<ReplicationResult>(rs);
 			
-			replication.add(e)
+			replication.add(task);
+			
+			//Create thread object using the task object created
+			Thread t = new Thread(task);
+			t.start();
 		}
-		// TODO: Implement this method
-		return null;
+		
+		
+		return replication;
 	}
 
 }
